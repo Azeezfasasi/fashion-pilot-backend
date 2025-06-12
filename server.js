71b121e67bc8ebb74530c974dce716d9c46467c2
azeezfasasi <fas.azeez@gmail.com>
2025-06-12 02:15:11 +0100

    corected

--- server.js
+++ server.js
@@ -11,7 +11,18 @@ const applicationRoutes = require('./routes/applicationRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+const cors = require('cors');
+
+app.use(cors({
+  origin: [
+    'http://localhost:5000', // your local frontend
+    'http://localhost:5173', // Vite default
+    'https://fashion-pilot-backend.onrender.com' // your deployed frontend
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan('dev'));
 
